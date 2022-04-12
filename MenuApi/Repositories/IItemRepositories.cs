ac4aea074644235d0bd5f21e05ea137f8f4e896d
Mete Gultekin <metegultekin98@hotmail.com>
2022-04-12 17:58:00 +0300

    4th

--- MenuApi/Repositories/IItemRepositories.cs
+++ MenuApi/Repositories/IItemRepositories.cs
@@ -8,7 +8,8 @@ namespace MenuApi.Repositories
     {
         Item GetItem(Guid id);
         IEnumerable<Item> GetItems();
-
         void CreateItem(Item item);
+        void UpdateItem(Item item);
+        void DeleteItem(Guid id);
     }
 }
