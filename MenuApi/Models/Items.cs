4434dd73ba222ecfc2636a9656807a219e6eeda5
Mete Gultekin <metegultekin98@hotmail.com>
2022-04-13 18:14:28 +0300

    6th

---
+++ MenuApi/Models/Items.cs
@@ -0,0 +1,19 @@
+﻿namespace MenuApi.Models
+{
+    public class ItemsModel
+    {
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string SeName { get; set; }
+        public int ProductId { get; set; }
+        public string OldPrice { get; set; }
+        public decimal OldPriceValue { get; set; }
+        public string Price { get; set; }
+        public string PriceWithDiscount { get; set; }
+        public decimal PriceValue { get; set; }
+        public decimal PriceWithDiscountValue { get; set; }
+        public float SavingPercent { get; set; }
+        public string SavingAmount { get; set; }
+    }
+}
